857c49b23c79f9df34ddc89a90136945000e64af
Karina paudel <karinapaudel6@gmail.com>
2025-06-13 17:19:55 +0545

    Day 2: Flutter workshop progress on June 13, 2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,10 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:demoapp/widgets/custom_button.dart';//for ios design
 
 void main() {
-  runApp(const MyApp()); // This one is used
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -33,132 +13,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Final App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Karina Demo Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-List<String> foods = ['Pizza', 'Burger', 'Momo', 'Thukpa'];
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('my app bar'),
+        ),
+        body: Column(
           children: [
-            const SizedBox(height: 20),
-            ListView.builder(
-              itemCount: foods.length,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: Text(foods[index], style: TextStyle(fontSize: 18)),
-                );
-              },
+            SizedBox(
+              height: 40,
             ),
-            const SizedBox(height: 20),
-            const Text(
-              "Hello my name is Karina",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            CustomButton(title: "Karina",decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(12)
+            ),),
+            SizedBox(
+              height: 40,
             ),
-            const SizedBox(height: 20),
-            InkWell(
-              onTap: () {
-                print("The button is pressed");
-              },
-              child: Container(
-                height: 54,
-                width: 140,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: const Center(
-                  child: Text(
-                    "Press me",
-                    style: TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: const Icon(Icons.phone, color: Colors.white, size: 50),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text("Accept"),
-                  ],
-                ),
-                const SizedBox(width: 40),
-                Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: const Icon(Icons.phone, color: Colors.white, size: 50),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text("Decline"),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            CustomButton(title: "Login",decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(12)
+            ),)
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        // body: Container(
+        //   color: Colors.white,
+        //   child: Row(
+        //     children: [
+        //     Text("data"),
+        //       Text("data"),
+        //       Text("data"),
+        //       Text("data"),
+        //       CircularProgressIndicator(),
+        //       ElevatedButton(onPressed: (){}, child: Text("data")),
+        //       CupertinoButton(child: Text("data"), onPressed: (){})
+        //     ],
+        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   ),
+        // ),
+        floatingActionButton: FloatingActionButton(onPressed: (){}),
       ),
     );
   }
