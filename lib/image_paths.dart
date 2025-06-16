c7dec578f1fe6a4e3b592c1f6baaea1e4f34ecdb
Karina paudel <karinapaudel6@gmail.com>
2025-06-16 19:10:36 +0545

    Added profile card

--- lib/image_paths.dart
+++ lib/image_paths.dart
@@ -2,4 +2,5 @@ class ImagePaths{
   static const baseUrl="assets/images";
 
   static const dogImage = "$baseUrl/dog.jpg";
+  static const profileImage = "$baseUrl/profile2.jpg";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:demoapp/image_paths.dart';
+import 'image_paths.dart';
 import 'package:flutter/material.dart';
-
 import 'colors.dart';
 
 void main() {
@@ -19,24 +18,129 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
-        title: const Text('Dynamic Buttons'),
+        title: Text(''),backgroundColor: AppColors.backgroundColor,
         centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset(ImagePaths.profileImage,height: 120,width: 120,
+                    ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Jane_Doe",
+                    style: TextStyle(
+                    color: AppColors.textColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        ProfileButtons(title: "Edit Profile",),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        ProfileButtons(title: "Share",),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "31 posts",
+                          style: TextStyle(
+                            fontSize: 16,
+                              fontWeight: FontWeight.w800,
+                            color: AppColors.textColor
+                        ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "599 followers",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w800,
+                              color: AppColors.textColor
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "200 following",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w800,
+                              color: AppColors.textColor
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text("Jane Doe",
+                      style:TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.textColor,
+                    ),
+                    )
+                  ],
+                ),
+              ],
+            )
             // Image.network("https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 70),
-              child: Image.asset(ImagePaths.dogImage),
-            ),
-            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-           ElevatedButton(onPressed: () {}, child: Text("data"))
-            // Image.asset(ImagePaths.dogImage),
-            // Image.asset(ImagePaths.dogImage),
+           //  Padding(
+           //    padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 70),
+           //    child: Image.asset(ImagePaths.dogImage),
+           //  ),
+           //  IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+           // ElevatedButton(onPressed: () {}, child: Text("data"))
+           //  // Image.asset(ImagePaths.dogImage),
+           //  // Image.asset(ImagePaths.dogImage),
           ],
         ),
+      ),
+      ),
+    );
+  }
+}
+
+class ProfileButtons extends StatelessWidget {
+  final String title;
+  const ProfileButtons({
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 12) ,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: AppColors.secondaryColor,
+      ),
+      child: Text(
+        "$title",
+        style: TextStyle(
+        color: AppColors.textColor,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
       ),
       ),
     );
